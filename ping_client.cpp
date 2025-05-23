8bc02963f033959e45f14ec7c4bf54bbd97e9e99
goshanraj-g <govindag@mcmaster.ca>
2025-05-22 21:47:33 -0400

    finished basic server + client implementation

--- ping_client.cpp
+++ ping_client.cpp
@@ -14,15 +14,34 @@
 
 int main()
 {
+    // initialize client socket, setup with IPV4, and define TCP, while letting the OS choose the default protocol (0)
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // initialize serverAddress to be of sockaddr_in data type
     sockaddr_in serverAddress;
+
+    // setup for IPv4
     serverAddress.sin_family = AF_INET;
+
+    // convert the port into something that's readable for serverAddress
     serverAddress.sin_port = htons(8080);
+
+    // make server listen to all possible IPs
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
+    // connect socket to server @ struct sockaddr* &serverAddress (pointer to IP + Port)
     connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 
-    const char *message = "hello, server";
+    // sends message from clientSocket
+    const char *message = "Server Connected!";
     send(clientSocket, message, strlen(message), 0);
+
+    std::string userMessage;
+    std::cout << "Enter command";
+    std::getline(std::cin, userMessage);
+
+    // ensure that the message parameter is a pointer to the character array in st ring (this parameter is supposed to be a buffer)
+    send(clientSocket, userMessage.c_str(), userMessage.size(), 0);
+
     close(clientSocket);
 }
