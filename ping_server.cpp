96fe2508dacfa2935418c04f0d407a18c9b2f403
goshanraj-g <govindag@mcmaster.ca>
2025-05-22 22:59:24 -0400

    included multithreading + mutex

--- ping_server.cpp
+++ ping_server.cpp
@@ -18,6 +18,54 @@ constexpr uint16_t kPort = 10000;
 constexpr size_t kMaxLineLen = 100;
 // create message length
 
+std::mutex coutMutex;
+// initialize a lock -> only one thread at a time can hold the lock, without this, two threads can print out to the screen at the same time
+
+// runs in it's own thread for each client
+// handles all communication for each client
+void handleClient(int clientSocket)
+{
+    // buffer for incoming data
+    char buffer[1024];
+
+    while (true)
+    {
+        std::memset(buffer, 0, sizeof(buffer)); // clear buffer
+        ssize_t bytesRecieved = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytesRecieved <= 0)
+        {
+            // lock mutex when it runs, and unlocks when the loop is done
+            std::lock_guard<std::mutex> lock(coutMutex);
+            std::cout << "[Client disconnected]" << std::endl;
+            break;
+        }
+
+        buffer[bytesRecieved] = '\0';
+
+        // lock mutex to safely print to console
+        std::lock_guard<std::mutex> lock(coutMutex);
+
+        if (std::strcmp(buffer, "ping") == 0)
+        {
+            std::cout << "Received: ping -> Sending: pong\n";
+            send(clientSocket, "pong", 4, 0); // respond to client
+        }
+        else if (std::strcmp(buffer, "exit") == 0)
+        {
+            std::cout << "[Client requested disconnect]\n";
+            break; // exit loop
+        }
+        else
+        {
+            std::cout << "Received unknown command: " << buffer << "\n";
+            send(clientSocket, "Unknown Command", 16, 0); // respond with default
+        }
+    }
+
+    close(clientSocket);
+}
+
 void runServer()
 {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -64,51 +112,39 @@ void runServer()
     std::cout << "Server listening on port " << kPort << "...\n"
               << std::endl;
 
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
-    // accept() is used to accept the connection request which is recieved on the socket the application was listening to
-    // take first client from waiting queue on serverSocket, and give a new socket to talk to that client
-    // second and third parameter can be used to get the client's IP and port, but we don't necessarily need it
+    // below would be valid if we're only handling for one client
+    // int clientSocket = accept(serverSocket, nullptr, nullptr);
+    // // accept() is used to accept the connection request which is recieved on the socket the application was listening to
+    // // take first client from waiting queue on serverSocket, and give a new socket to talk to that client
+    // // second and third parameter can be used to get the client's IP and port, but we don't necessarily need it
 
-    if (clientSocket < 0)
-    {
-        std::cerr << "Accept failed: " << strerror(errno) << std::endl;
-        close(serverSocket);
-        return;
-    }
+    // if (clientSocket < 0)
+    // {
+    //     std::cerr << "Accept failed: " << strerror(errno) << std::endl;
+    //     close(serverSocket);
+    //     return;
+    // }
 
-    char buffer[1024];
+    // char buffer[1024];
 
     // create a constant loop which listens to client
     while (true)
     {
-        std::memset(buffer, 0, sizeof(buffer));
-        // clear buffer before each recieve request
-        ssize_t bytesRecieved = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-        // sizeof(buffer) - 1 -> how many bytes recieve function should try to read, -1 is included because of the null terminator
-        // 0 -> read data as is
-        // NOTE: recv() does not automatically add a null terminator to count the end of the string
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        if (clientSocket < 0)
+        {
+            std::cerr << "Accept failed: " << strerror(errno) << std::endl;
+            continue;
+        }
 
-        if (bytesRecieved <= 0)
         {
-            std::cout << "Client disconnected or error has occured. \n";
-            break;
+            std::lock_guard<std::mutex> lock(coutMutex);
+            std::cout << "[New client connected]" << std::endl;
         }
-        buffer[bytesRecieved] = '\0';
-        // this is required to indicate the end of a string
-
-        if (std::strcmp(buffer, "ping") == 0)
-        {
-            std::cout << "Recieved: ping -> Sending: pong\n";
-            send(clientSocket, "pong", 4, 0);
-        }
-        else
-        {
-            std::cout << "Recieved unknown command: " << buffer << "\n";
-            send(clientSocket, "Unknown Command", 16, 0);
-        }
-    }
-    close(clientSocket);
-    close(serverSocket);
+        // start a new thread, and run the handleClient function, and provide the clientSocket to work with... all while running it in the background (doesn't matter when this is finished)
+        // allows multiple clients, without waiting for old ones to finish, and handle each one in isolation
+        std::thread(handleClient, clientSocket).detach();
+    };
 }
 
 int main()
