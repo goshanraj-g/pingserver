df47d7acae91c8de0e880c20186415e592816453
goshanraj-g <govindag@mcmaster.ca>
2025-05-22 22:33:53 -0400

    finished ping->pong implementation

--- ping_client.cpp
+++ ping_client.cpp
@@ -12,36 +12,76 @@
 #include <unistd.h>     // For close() to close sockets
 #include <errno.h>      // For error codes like errno
 
+constexpr const char *kServerIP = "127.0.0.1";
+constexpr uint16_t kPort = 10000;
+
 int main()
 {
     // initialize client socket, setup with IPV4, and define TCP, while letting the OS choose the default protocol (0)
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0)
+    {
+        std::cerr << "Failed to create socket" << std::endl;
+    }
 
     // initialize serverAddress to be of sockaddr_in data type
-    sockaddr_in serverAddress;
+    sockaddr_in serverAddress{};
+    // make serverAddress a uniform initialization which zero-intiailizes all fields of struct
 
     // setup for IPv4
     serverAddress.sin_family = AF_INET;
 
     // convert the port into something that's readable for serverAddress
-    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_port = htons(kPort);
 
-    // make server listen to all possible IPs
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    // make server listen to the IP
+    if (inet_pton(AF_INET, kServerIP, &serverAddress.sin_addr) <= 0)
+    {
+        std::cerr << "Invalid address or address not supported" << std::endl;
+        return 1;
+    }
 
     // connect socket to server @ struct sockaddr* &serverAddress (pointer to IP + Port)
-    connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        std::cerr << "Connection failed" << std::endl;
+        return 1;
+    }
 
     // sends message from clientSocket
-    const char *message = "Server Connected!";
-    send(clientSocket, message, strlen(message), 0);
+    // const char *message = "Server Connected!";
+    // send(clientSocket, message, strlen(message), 0);
 
-    std::string userMessage;
-    std::cout << "Enter command";
-    std::getline(std::cin, userMessage);
+    char buffer[1024];
+    std::string input;
+    while (true)
+    {
+        std::cout << "> ";
+        std::getline(std::cin, input);
 
-    // ensure that the message parameter is a pointer to the character array in st ring (this parameter is supposed to be a buffer)
-    send(clientSocket, userMessage.c_str(), userMessage.size(), 0);
+        // input.c_str() must be done to turn input into a pointer, as send needs the raw char array
+        send(clientSocket, input.c_str(), input.length(), 0);
 
+        if (input == "exit")
+        {
+            break;
+        }
+        // else if (input == "ping")
+        // {
+        //     std::cout << "pong" << std::endl;
+        // }
+        std::memset(buffer, 0, sizeof(buffer));
+        ssize_t bytesRecieved = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytesRecieved <= 0)
+        {
+            std::cout << "Server disconnected" << std::endl;
+            break;
+        }
+        buffer[bytesRecieved] = '\0';
+        std::cout << "Server: " << buffer << "\n";
+    }
     close(clientSocket);
+    return 0;
 }
--- ping_server.cpp
+++ ping_server.cpp
@@ -17,39 +17,102 @@ constexpr uint16_t kPort = 10000;
 
 constexpr size_t kMaxLineLen = 100;
 // create message length
-int main()
+
+void runServer()
 {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     // AF_INET -> IPv4 protocol family
     // SOCK_STREAM -> defines TCP type socket
 
-    sockaddr_in serverAddress;
+    if (serverSocket == -1)
+    {
+        std::cerr << "Failed to create socket\n"
+                  << std::endl;
+        return;
+    }
+    // error handling
+
+    sockaddr_in serverAddress{};
     // data type to store socket address
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_port = htons(kPort);
     // Host TO Network Short -> converts 16 bit integer (port number) to hosts's byte order (ensures everyone on the internet understands the numbers the same way)
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     // don't bind socket to any particular IP, and make it listen to all available IPs
 
-    bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        std::cerr << "Bind failed: " << strerror(errno) << std::endl;
+        close(serverSocket);
+        return;
+    }
+    // when if statement is run, the bind is already created, if there's an issue, then it will be handled by the error handler
     // without bind, os won't know port number, and ip address/interface it sohuld be bound to
     // tells OS which IP and port it belongs to so it can be binded
     // serverSocket (int) -> id number for socket
     // (struct sockaddr*)&serverAddress (casted pointer) -> pointer to sockaddr_in structure
     // sizeof(serverAddress) (socklen_t) -> tells os how big the address struct is, needed so it knows how much memory to read
 
-    listen(serverSocket, 5);
+    if (listen(serverSocket, 5) < 0)
+    {
+        std::cerr << "Listen failed: " << strerror(errno) << std::endl;
+        close(serverSocket);
+        return;
+    }
     // tell application to listen to the socket referred by the serverSocket, and if more than 5 people try to connect at once, their connection will be refused/delayed
+
+    std::cout << "Server listening on port " << kPort << "...\n"
+              << std::endl;
 
     int clientSocket = accept(serverSocket, nullptr, nullptr);
     // accept() is used to accept the connection request which is recieved on the socket the application was listening to
     // take first client from waiting queue on serverSocket, and give a new socket to talk to that client
     // second and third parameter can be used to get the client's IP and port, but we don't necessarily need it
 
-    char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    std::cout << "Message from client: " << buffer << std::endl;
-    // recieve data from client, buffer -> space to recieve the data sent to the client
+    if (clientSocket < 0)
+    {
+        std::cerr << "Accept failed: " << strerror(errno) << std::endl;
+        close(serverSocket);
+        return;
+    }
 
+    char buffer[1024];
+
+    // create a constant loop which listens to client
+    while (true)
+    {
+        std::memset(buffer, 0, sizeof(buffer));
+        // clear buffer before each recieve request
+        ssize_t bytesRecieved = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        // sizeof(buffer) - 1 -> how many bytes recieve function should try to read, -1 is included because of the null terminator
+        // 0 -> read data as is
+        // NOTE: recv() does not automatically add a null terminator to count the end of the string
+
+        if (bytesRecieved <= 0)
+        {
+            std::cout << "Client disconnected or error has occured. \n";
+            break;
+        }
+        buffer[bytesRecieved] = '\0';
+        // this is required to indicate the end of a string
+
+        if (std::strcmp(buffer, "ping") == 0)
+        {
+            std::cout << "Recieved: ping -> Sending: pong\n";
+            send(clientSocket, "pong", 4, 0);
+        }
+        else
+        {
+            std::cout << "Recieved unknown command: " << buffer << "\n";
+            send(clientSocket, "Unknown Command", 16, 0);
+        }
+    }
+    close(clientSocket);
     close(serverSocket);
+}
+
+int main()
+{
+    runServer();
+    return 0;
 }
